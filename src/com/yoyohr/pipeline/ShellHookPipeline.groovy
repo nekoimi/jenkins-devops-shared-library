65b8b0f68ceffbdc8109df0fcb4e714b9d53490e
nekoimi <nekoimime@gmail.com>
2022-05-29 12:58:49 +0800

    up

--- src/com/yoyohr/pipeline/ShellHookPipeline.groovy
+++ src/com/yoyohr/pipeline/ShellHookPipeline.groovy
@@ -9,33 +9,37 @@ class ShellHookPipeline extends AbstractPipeline {
 
     @java.lang.Override
     void build() {
-        notice.noticeWarning("""
-Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 Build 流程。
-Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
-""")
+        invokeMethod("noticeWarning", "aaa")
+//        notice.noticeWarning("""
+//Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 Build 流程。
+//Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
+//""")
     }
 
     @java.lang.Override
     void dockerImage() {
-        notice.noticeWarning("""
-Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 DockerImage 流程。
-Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
-""")
+        invokeMethod("noticeWarning", "aaa")
+//        notice.noticeWarning("""
+//Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 DockerImage 流程。
+//Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
+//""")
     }
 
     @java.lang.Override
     void dockerPush() {
-        notice.noticeWarning("""
-Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 dockerPush 流程。
-Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
-""")
+        invokeMethod("noticeWarning", "aaa")
+//        notice.noticeWarning("""
+//Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 dockerPush 流程。
+//Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
+//""")
     }
 
     @java.lang.Override
     void deploy() {
-        notice.noticeWarning("""
-Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 deploy 流程。
-Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
-""")
+        invokeMethod("noticeWarning", "aaa")
+//        notice.noticeWarning("""
+//Warning！当前项目 project.yaml 配置文件不存在，请使用 Hook 完成 deploy 流程。
+//Hook 使用参见：http://code-base.yoyohr.com/kubernetes/no-jenkinsfile
+//""")
     }
 }
