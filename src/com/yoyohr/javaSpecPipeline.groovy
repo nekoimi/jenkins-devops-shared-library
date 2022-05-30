54978eeb5c103d192a0bf4100b49c2ef7d0305c0
nekoimi <nekoimime@gmail.com>
2022-05-30 15:04:14 +0800

    update

--- src/com/yoyohr/javaSpecPipeline.groovy
+++ src/com/yoyohr/javaSpecPipeline.groovy
@@ -82,7 +82,7 @@ def deployTestToKubernetes() {
         sshCommand remote: server, command: """
 bash -ex;
 
-chartStatus=\$(helm list --all --time-format "2006-01-02" --filter "${MY_PROJECT_NAME}" | sed -n '2p' | awk '{print \$5}')
+chartStatus=\$(helm list --all --time-format "2006-01-02" --filter "\${MY_PROJECT_NAME}" | sed -n '2p' | awk '{print \$5}')
 
 echo chartStatus
 """
