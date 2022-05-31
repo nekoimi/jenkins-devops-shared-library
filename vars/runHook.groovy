d50ef97c4a2d09ae5d5a2ecdbd4f41f015eb8971
nekoimi <nekoimime@gmail.com>
2022-05-31 21:25:12 +0800

    fixbug

--- vars/runHook.groovy
+++ vars/runHook.groovy
@@ -4,15 +4,15 @@
  * @author nekoimi 2022/05/31
  */
 
-def call(yamlConf, hook, envCommandExec) {
-    def hook = dataGet(yamlConf, "pipelineHook.${hook}")
-    if (stringIsNotEmpty(hook)) {
+def call(yamlConf, hook, commandExec) {
+    def hookCommand = dataGet(yamlConf, "pipelineHook.${hook}")
+    if (stringIsNotEmpty(hookCommand)) {
         withEnv([
-                "MY_COMMAND_EXEC=${envCommandExec}"
+                "MY_COMMAND_EXEC=${commandExec}"
         ]) {
             sh """
 bash -ex;
-${hook}
+${hookCommand}
 """
         }
     }
