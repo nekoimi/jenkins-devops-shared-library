38b9b8d684ff0e72eddf5140e1350fdfce4806e8
nekoimi <nekoimime@gmail.com>
2022-06-06 11:42:32 +0800

    update php

--- src/com/yoyohr/phpSpecPipeline.groovy
+++ src/com/yoyohr/phpSpecPipeline.groovy
@@ -5,9 +5,47 @@ package com.yoyohr
  * @author nekoimi 2022/05/29
  */
 
+def doBuild(command) {
+    sh """
+bash -ex;
+
+\${MY_COMMAND_EXEC} ${command}
+
+ls -l
+"""
+}
+
 def build(yamlConf) {
-    runHook(yamlConf, "buildBefore", "")
-    runHook(yamlConf, "buildAfter", "")
+    def buildImage = dataGet(yamlConf, "buildImage")
+    if (stringIsNotEmpty(buildImage)) {
+        def installCommand = dataGet(yamlConf, "installCommand")
+        def buildCommand = dataGet(yamlConf, "buildCommand")
+        // commandExec
+        def commandExec = "docker run --rm -w /workspace -v /root/.composer:/root/.composer -v ${MY_PWD}:/workspace ${buildImage}"
+
+        runHook(yamlConf, "buildBefore", commandExec)
+
+        if (stringIsNotEmpty(installCommand)) {
+            withEnv([
+                    "MY_COMMAND_EXEC=${commandExec}"
+            ]) {
+                doBuild(installCommand)
+            }
+        }
+
+        if (stringIsNotEmpty(buildCommand)) {
+            withEnv([
+                    "MY_COMMAND_EXEC=${commandExec}"
+            ]) {
+                doBuild(buildCommand)
+            }
+        }
+
+        runHook(yamlConf, "buildAfter", commandExec)
+    } else {
+        runHook(yamlConf, "buildBefore", "")
+        runHook(yamlConf, "buildAfter", "")
+    }
 }
 
 def unitTesting(yamlConf) {
